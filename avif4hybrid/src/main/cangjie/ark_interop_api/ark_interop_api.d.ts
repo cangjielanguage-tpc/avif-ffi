98b06801ab6a990aedc8cba467b17339ddb2f913
huanghaoshuai <739606538@qq.com>
2026-01-04 20:16:35 +0800

    提交avif互操作代码

--- avif4hybrid/src/main/cangjie/ark_interop_api/ark_interop_api.d.ts
+++ avif4hybrid/src/main/cangjie/ark_interop_api/ark_interop_api.d.ts
@@ -1,3 +1,20 @@
+export declare class CJCjinfo {
+    width: number
+    height: number
+    depth: number
+    alphaPresent: boolean
+    frameCount: number
+    repetitionCount: number
+    frameDurations: Array<number>
+    setWidth(width: number): void
+    setHeight(height: number): void
+    setDepth(depth: number): void
+    setAlphaPresent(alphaPresent: boolean): void
+    setFrameCount(frameCount: number): void
+    setRepetitionCount(repetitionCount: number): void
+    setFrameDurations(frameDurations: Array<number>): void
+}
+
 export declare class CJAvifImageModel {
     setImageBuffer(imageBuffer: ArrayBuffer): void
     setImagePicWidth(imagePicWidth: number): void
@@ -13,6 +30,27 @@ export declare class CJAvifImageModel {
     setCompleteCallback(completeCallBack: (funcArg0: number) => void): void
 }
 
+export declare class CJAvifDecoder {
+    create(encoded: ArrayBuffer): CJAvifDecoder | undefined
+    createWithThread(encoded: ArrayBuffer, threads: number): CJAvifDecoder | undefined
+    getWidth(): number
+    getHeight(): number
+    getDepth(): number
+    getAlphaPresent(): boolean
+    getFrameCount(): number
+    getRepetitionCount(): number
+    getFrameDurations(): Array<number> | undefined
+    isAvifImageffi(encoded: ArrayBuffer): number
+    getInfoffi(encoded: ArrayBuffer): CJCjinfo | undefined
+    nextFrameIndexffi(): number
+    nextFrameffi(color: ArrayBuffer, imageWidth: number, imageHeight: number): number
+    nthFrameffi(index: number, color: ArrayBuffer, imageWidth: number, imageHeight: number): number
+    decodeffi(encoded: ArrayBuffer, color: ArrayBuffer, imageWidth: number, imageHeight: number): boolean
+    release(): void
+}
+
 export declare interface CustomLib {
+    CJAvifDecoder: {new (): CJAvifDecoder}
     CJAvifImageModel: {new (): CJAvifImageModel}
+    CJCjinfo: {new (): CJCjinfo}
 }
--- avif4hybrid/src/main/cangjie/types/libohos_app_cangjie_avif4hybrid/Index.d.ts
+++ avif4hybrid/src/main/cangjie/types/libohos_app_cangjie_avif4hybrid/Index.d.ts
@@ -1,3 +1,41 @@
+export declare class CJAvifDecoder {
+    create(encoded: ArrayBuffer): CJAvifDecoder | undefined
+    createWithThread(encoded: ArrayBuffer, threads: number): CJAvifDecoder | undefined
+    getWidth(): number
+    getHeight(): number
+    getDepth(): number
+    getAlphaPresent(): boolean
+    getFrameCount(): number
+    getRepetitionCount(): number
+    getFrameDurations(): Array<number> | undefined
+    isAvifImageffi(encoded: ArrayBuffer): number
+    getInfoffi(encoded: ArrayBuffer): CJCjinfo | undefined
+    nextFrameIndexffi(): number
+    nextFrameffi(color: ArrayBuffer, imageWidth: number, imageHeight: number): number
+    nthFrameffi(index: number, color: ArrayBuffer, imageWidth: number, imageHeight: number): number
+    decodeffi(encoded: ArrayBuffer, color: ArrayBuffer, imageWidth: number, imageHeight: number): boolean
+    release(): void
+    constructor ()
+}
+
+export declare class CJCjinfo {
+    width: number
+    height: number
+    depth: number
+    alphaPresent: boolean
+    frameCount: number
+    repetitionCount: number
+    frameDurations: Array<number>
+    setWidth(width: number): void
+    setHeight(height: number): void
+    setDepth(depth: number): void
+    setAlphaPresent(alphaPresent: boolean): void
+    setFrameCount(frameCount: number): void
+    setRepetitionCount(repetitionCount: number): void
+    setFrameDurations(frameDurations: Array<number>): void
+    constructor ()
+}
+
 export declare class CJAvifImageModel {
     setImageBuffer(imageBuffer: ArrayBuffer): void
     setImagePicWidth(imagePicWidth: number): void
@@ -23,6 +61,8 @@ import { Context } from "@kit.AbilityKit"
 
 export declare interface CustomLib {
     CJAvifImageModel: {new (): CJAvifImageModel}
+    CJAvifDecoder: {new (): CJAvifDecoder}
+    CJCjinfo: {new (): CJCjinfo}
     getLoadCJPage(model: CJAvifImageModel): void
 }
 
