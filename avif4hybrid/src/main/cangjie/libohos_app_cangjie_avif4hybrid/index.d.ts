3cddb6d5ba7ea871649519a0f45579d5da39062c
huanghaoshuai <739606538@qq.com>
2026-03-06 11:23:40 +0800

    提交测试过后的缓存版本

--- avif4hybrid/src/main/cangjie/libohos_app_cangjie_avif4hybrid/index.d.ts
+++ avif4hybrid/src/main/cangjie/libohos_app_cangjie_avif4hybrid/index.d.ts
@@ -1,11 +1,14 @@
+import { Context } from "@kit.AbilityKit"
+
 export declare class CJReturnValue {
     code: number
     color: ArrayBuffer | undefined
     issuccess: boolean
+    constructor ()
 }
 
 export declare class CJAvifImageModel {
-    setImageBuffer(imageBuffer: ArrayBuffer): void
+    setUri(uri: string): void
     setImagePicWidth(imagePicWidth: number): void
     setImagePicHeight(imagePicHeight: number): void
     setImageViewWidth(imageViewWidth: string): void
@@ -17,11 +20,12 @@ export declare class CJAvifImageModel {
     setBottomleftBorderRadius(bottomleftBorderRadius: string): void
     setBottomrightBorderRadius(bottomrightBorderRadius: string): void
     setCompleteCallback(completeCallBack: (funcArg0: number) => void): void
+    constructor ()
 }
 
 export declare class CJAvifDecoder {
-    create(encoded: ArrayBuffer): CJAvifDecoder | undefined
-    createWithThread(encoded: ArrayBuffer, threads: number): CJAvifDecoder | undefined
+    create(uri: string): CJAvifDecoder | undefined
+    createWithThread(uri: string, threads: number): CJAvifDecoder | undefined
     getWidth(): number
     getHeight(): number
     getDepth(): number
@@ -29,18 +33,20 @@ export declare class CJAvifDecoder {
     getFrameCount(): number
     getRepetitionCount(): number
     getFrameDurations(): Array<number> | undefined
-    isAvifImageffi(encoded: ArrayBuffer): number
+    isAvifImageffi(): number
     nextFrameIndexffi(): number
     nextFrameffi(imageWidth: number, imageHeight: number): CJReturnValue
     nthFrameffi(index: number, imageWidth: number, imageHeight: number): CJReturnValue
-    decodeffi(encoded: ArrayBuffer, imageWidth: number, imageHeight: number): CJReturnValue
+    decodeffi(imageWidth: number, imageHeight: number): CJReturnValue
     release(): void
+    constructor ()
 }
 
 
-export declare interface CustomLib {
-    CJAvifDecoder: {new (): CJAvifDecoder}
-    CJAvifImageModel: {new (): CJAvifImageModel}
-    CJReturnValue: {new (): CJReturnValue}
-    getLoadCJPage(model: CJAvifImageModel): void
-}
+export declare function setGlobalContext(context: Context): void
+
+export declare function getLoadCJPage(model: CJAvifImageModel): void
+
+export declare function setMemoryLruCache(countSize:number,memorySize:number): void
+
+export declare function setFileLruCache(countSize:number,memorySize:number): void
